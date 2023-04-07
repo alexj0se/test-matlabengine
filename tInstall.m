03a1426df9c6e9869411f0be0447563b818b6e87
alexjose <alexjose@MATHWORKS-iTQ2V.dhcp.mathworks.com>
2023-04-07 14:14:49 -0400

    Add test points

--- tInstall.m
+++ tInstall.m
@@ -1,8 +1,30 @@
 classdef tInstall < matlab.unittest.TestCase
+% Verify installation of matlab engine
+
+    properties (Constant)
+        MATLABVersion = string(ver().Version)
+        MATLABRelease = erase(ver().Release,{'(',')'})
+    end
 
     methods (Test)
-        function install(testCase)
+        function installNoVersionSpecified(testCase)
+            assumeEqual(testCase, testCase.MATLABRelease, 'R2023a')
             [status, out] = system("pip install matlabengine")
+			verifyEqual(testCase, status, 0);
+
+        end
+
+        function installMatchingEngine(testCase)
+            [status, out] = system("pip install matlabengine==" + testCase.MATLABVersion + ".*")
+			verifyEqual(testCase, status, 0);
+        end
+
+        function installEarlierEngine(testCase)
+            earlierVersion = extractBefore(testCase.MATLABVersion, '.') + "." + ...
+                (str2num(extractAfter(testCase.MATLABVersion, '.')) - 1) + ".*";
+            [status, out] = system("pip install matlabengine==" + earlierVersion)
+			verifyNotEqual(testCase, status, 0, "Install must fail");
         end
     end
+
 end
