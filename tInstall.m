be7ed5ac61b230ace8d88c25c9bbe9a22a3d9314
alexjose <alexjose@MATHWORKS-iTQ2V.dhcp.mathworks.com>
2023-05-01 12:03:22 -0400

    Add verification and limit to linux

--- tInstall.m
+++ tInstall.m
@@ -9,22 +9,39 @@ classdef tInstall < matlab.unittest.TestCase
     methods (Test)
         function installNoVersionSpecified(testCase)
             assumeEqual(testCase, testCase.MATLABRelease, 'R2023a')
-            [status, out] = system("pip install matlabengine")
-			verifyEqual(testCase, status, 0);
-
+            [status, out] = system("pip install matlabengine");
+			verifyEqual(testCase, status, 0, out)
+			verifyInstallation(testCase)
         end
 
         function installMatchingEngine(testCase)
-            [status, out] = system("pip install matlabengine==" + testCase.MATLABVersion + ".*")
-			verifyEqual(testCase, status, 0);
+            [status, out] = system("pip install matlabengine==" + testCase.MATLABVersion + ".*");
+			verifyEqual(testCase, status, 0, out)
+			verifyInstallation(testCase)
         end
 
         function installEarlierEngine(testCase)
             earlierVersion = extractBefore(testCase.MATLABVersion, '.') + "." + ...
-                (str2num(extractAfter(testCase.MATLABVersion, '.')) - 1) + ".*";
-            [status, out] = system("pip install matlabengine==" + earlierVersion)
-			verifyNotEqual(testCase, status, 0, "Install must fail");
+                (str2double(extractAfter(testCase.MATLABVersion, '.')) - 1) + ".*";
+            [status, out] = system("pip install matlabengine==" + earlierVersion);
+			verifyNotEqual(testCase, status, 0, "Install must fail. Output:" + newline + out)
         end
     end
 
+	properties
+		SharedEngineName(1,1) string 
+	end
+
+	methods
+		function verifyInstallation(testCase)
+		% Verify installation by calling functions in matlab engine
+		% Share this session and see if find_matlab can find it.
+			if isempty(testCase.SharedEngineName)
+				testCase.SharedEngineName = matlab.lang.makeValidName(tempname);
+				matlab.engine.shareEngine(testCase.SharedEngineName)
+			end
+			pySharedEngineName = string(py.matlab.engine.find_matlab());
+			verifyEqual(testCase, pySharedEngineName, testCase.SharedEngineName)
+        end
+	end
 end
